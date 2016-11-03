0210dcd22d8ea435a3b0d44fa1ce14b18f3f186c
Brandon <bcooper94@github.com>
2016-11-03 10:28:12 -0700

    Created CandidateMailbox and Message classes to store message/mailbox info

--- CampaignEmails.py
+++ CampaignEmails.py
@@ -3,13 +3,39 @@ import os
 import mailbox
 
 
+class CandidateMailbox:
+    def __init__(self, fileName: str):
+        candidateParty = extractName(fileName)
+        self.party = candidateParty[0]
+        self.candidate = candidateParty[1]
+        self._mailbox = mailbox.mbox(fileName)
+        self.messages = [Message(message) for message in self._mailbox]
+
+    def __str__(self):
+        return '<CandidateMailbox candidate={}, party={}'.format(self.candidate, self.party)
+
+
+class Message:
+    def __init__(self, message: mailbox.mboxMessage):
+        self.body = message.get_payload()
+        self._mboxMessage = message
+        self.headers = {}
+
+        for header, value in message.items():
+            self.headers[header] = value
+
+
 def extractName(fileName: str):
-    names = fileName.split('-')
+    if '/' in fileName:
+        names = fileName.split('/')[-1]
+    else:
+        names = fileName
+    names = names.split('-')
     return names[0], names[1].split('.mbox')[0]
 
 
 def extractMboxes(targetDir: str):
-    return [(extractName(file), mailbox.mbox('{}/{}'.format(path, file)))
+    return [CandidateMailbox('{}/{}'.format(path, file))
             for (path, dirs, allFiles)
             in os.walk(targetDir) for file in allFiles if
             file.endswith('.mbox')]
@@ -20,11 +46,8 @@ def main():
         mboxes = extractMboxes(sys.argv[1])
     else:
         mboxes = extractMboxes('Takeout/Mail/')
-    print(mboxes)
-    # All headers:
-    print(mboxes[0][1][0].items())
-    # Message body:
-    print(str(mboxes[0][1][0].get_payload()[0]))
+    for mbox in mboxes:
+        print(mbox)
 
 
 if __name__ == '__main__':
